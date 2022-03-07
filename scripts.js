3cc5e0a3a392c58e357639f87a390d474ebd234d
CynthiaWairimu <wairimucee@gmail.com>
2022-03-07 17:17:37 +0300

    Edit stysheet in the styles.css file.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
 function birthDate(){
     var dayWeek=document.getElementById("day").value;
      var dd=parseInt(dayWeek);
+
     var month=document.getElementById("month").value;
       var mm=parseInt(month);
+
     var year=document.getElementById("year").value;
       var yy=parseInt(year.substr(2,4));
       var cc=parseInt(year.substr(0,2));
+
     var day=parseInt(((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10))+ dd)%7;
+
     var gender=document.getElementById("gender").value
-    
+
     //Validating the form
     if (dd<=0||dd>31||dd ==""){
         alert('Enter a valid date');
